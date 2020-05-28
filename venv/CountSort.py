a4ded8be7a9da5aa2df867dd926ca7c26bac2c39
DuTogira <at.smothers@gmail.com>
2020-05-28 15:05:10 -0700

    Enable support for floats
    Allow the algorithm to handle and properly sort decimal input.
    Additionally, fix the stability of the algorithm, which was
    previously broken.

--- venv/CountSort.py
+++ venv/CountSort.py
@@ -1,11 +1,13 @@
 # Combined complexity of improved count sort algorithm:
 # Runtime Complexity: O(n) best, O(n + k*log(k)) average, O(n*log(n)) worst when k == n
 # Space Complexity: O(n) best/average/worst
+# Note that for the purpose of analysis, k is the number of distinct values in our input
 
-# Note that this count sort handles negative integers, characters, and symbols.
-# This algorithm will not handle non-integer numbers correctly, though it technically sorts them
+# Note that this count sort handles positive/negative floats, characters, and symbols.
+# Non-float characters are sorted based on their ASCII integer value.
 
-# Since this algorithm is composed of count sort combined with TimSort, it is stable
+# Since this algorithm is composed of count sort combined with TimSort, it is stable,
+# meaning that same-value input maintains its order as output.
 
 base = 35
 
@@ -14,10 +16,8 @@ def duSort(a):
     # Space complexity copying input array: 0(n)
     aux = []
     for num in a:
-        if num.lstrip('-').isdigit():
-            aux.append(int(num))
-        elif num.isalnum():
-            aux.append(int(num, base))
+        if num.lstrip('-').isdigit() or num.replace('.', '').isdigit():
+            aux.append(float(num))
         else:
             aux.append(sum(int(c) if c.isdigit() else ord(c) for c in num))
     count_nums = dict()
@@ -33,24 +33,22 @@ def duSort(a):
     # Runtime adjusting count_nums to contain output positions: O(k) where k <= n
     # Space Complexity: O(1)
     prev_val = 0
-    # The python sorted() method takes O(k*log(k))... need to find a way to reduce this
+    curr = 0
+    # The python sorted() method takes O(k*log(k))
     for key in sorted(count_nums.keys()):
+        curr = count_nums[key] - 1
         count_nums[key] = count_nums[key] + prev_val
         prev_val = count_nums[key]
+        count_nums[key] -= curr
     # Runtime creating the sorted count array: O(n)
     # Space Complexity: O(1)
-    i = 0
-    for num in a:
-        if num.lstrip('-').isdigit():
-            aux[count_nums[int(num)] - 1] = a[i]
-            count_nums[int(num)] -= 1
-        elif num.isalnum():
-            aux[count_nums[int(num, base)] - 1] = a[i]
-            count_nums[int(num, base)] -= 1
+    for i, num in enumerate(a):
+        if num.lstrip('-').isdigit() or num.replace('.', '').isdigit():
+            aux[count_nums[float(num)] - 1] = a[i]
+            count_nums[float(num)] += 1
         else:
             aux[count_nums[sum(int(c) if c.isdigit() else ord(c) for c in num)] - 1] = a[i]
-            count_nums[sum(int(c) if c.isdigit() else ord(c) for c in num)] -= 1
-        i += 1
+            count_nums[sum(int(c) if c.isdigit() else ord(c) for c in num)] += 1
     a = aux
     return a
 
