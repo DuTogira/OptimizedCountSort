17b72a97fbab0cbaeb61379263b8f3397538bec1
DuTogira <at.smothers@gmail.com>
2020-05-28 15:25:50 -0700

    Improve Documentation
    Improve the comments for runtime and space complexity.

--- venv/CountSort.py
+++ venv/CountSort.py
@@ -1,9 +1,10 @@
 # Combined complexity of improved count sort algorithm:
-# Runtime Complexity: O(n) best, O(n + k*log(k)) average, O(n*log(n)) worst when k == n
-# Space Complexity: O(n) best/average/worst
-# Note that for the purpose of analysis, k is the number of distinct values in our input
+# For the purpose of analysis, k is the number of unique values in our input
+# Runtime Complexity: O(n + k*log(k))
+# O(n) best for sufficiently small k, O(n + k*log(k)) average, O(n*log(n)) worst when every value in our input is unique
+# Space Complexity: O(n) best/average/worst. Technically O(k + n) but k <= n always, reduces to O(n)
 
-# Note that this count sort handles positive/negative floats, characters, and symbols.
+# Note that this count sort handles positive/negative floats/integers, characters, and symbols.
 # Non-float characters are sorted based on their ASCII integer value.
 
 # Since this algorithm is composed of count sort combined with TimSort, it is stable,
@@ -20,11 +21,11 @@ def duSort(a):
             aux.append(float(num))
         else:
             aux.append(sum(int(c) if c.isdigit() else ord(c) for c in num))
-    count_nums = dict()
     # Runtime building count_nums: O(n)
     # Space Complexity: O(k) where k is the number of
     # distinct numbers within our input a
     # By definition, k <= n
+    count_nums = dict()
     for num in aux:
         if num in count_nums.keys():
             count_nums[num] += 1
@@ -34,8 +35,7 @@ def duSort(a):
     # Space Complexity: O(1)
     prev_val = 0
     curr = 0
-    # The python sorted() method takes O(k*log(k))
-    for key in sorted(count_nums.keys()):
+    for key in sorted(count_nums.keys()): # The python sorted() method takes O(k*log(k))
         curr = count_nums[key] - 1
         count_nums[key] = count_nums[key] + prev_val
         prev_val = count_nums[key]
@@ -51,6 +51,8 @@ def duSort(a):
             count_nums[sum(int(c) if c.isdigit() else ord(c) for c in num)] += 1
     a = aux
     return a
+    # Total runtime: O(n) + O(n) + O(k) + O(k*logk) + O(n) --> O(3n) + O(2k*logk) --> O(n + k*logk)
+    # Total space: O(n) + O(k) + O(1) + O(1) --> O(n + k) + 2 where k <= n always --> O(n)
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
