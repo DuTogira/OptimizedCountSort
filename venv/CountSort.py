d511ac4ce4d61752faf5dcb20b9d7f330e08332a
DuTogira <at.smothers@gmail.com>
2020-05-28 15:58:08 -0700

    Beautify the Algorithm

--- venv/CountSort.py
+++ venv/CountSort.py
@@ -5,17 +5,20 @@
 # Space Complexity: O(n) best/average/worst. Technically O(k + n) but k <= n always, reduces to O(n)
 
 # Note that this count sort handles positive/negative floats/integers, characters, and symbols.
-# Non-float characters are sorted based on their ASCII integer value.
+# Non-float characters are sorted based on the sum of their ASCII integer values.
 
 # Since this algorithm is composed of count sort combined with TimSort, it is stable,
 # meaning that same-value input maintains its order as output.
 
 base = 35
 
-def duSort(a):
-    # Runtime for copying input array: O(n)
-    # Space complexity copying input array: 0(n)
+def duSort(a: list):
+    prev_val = 0
+    curr = 0
     aux = []
+    count_nums = dict()
+    # Runtime for copying input array: O(n)
+    # Space complexity: 0(n)
     for num in a:
         if num.lstrip('-').isdigit() or num.replace('.', '').isdigit():
             aux.append(float(num))
@@ -25,7 +28,6 @@ def duSort(a):
     # Space Complexity: O(k) where k is the number of
     # distinct numbers within our input a
     # By definition, k <= n
-    count_nums = dict()
     for num in aux:
         if num in count_nums.keys():
             count_nums[num] += 1
@@ -33,8 +35,6 @@ def duSort(a):
             count_nums[num] = 1
     # Runtime adjusting count_nums to contain output positions: O(k) where k <= n
     # Space Complexity: O(1)
-    prev_val = 0
-    curr = 0
     for key in sorted(count_nums.keys()): # The python sorted() method takes O(k*log(k))
         curr = count_nums[key] - 1
         count_nums[key] = count_nums[key] + prev_val
@@ -49,11 +49,15 @@ def duSort(a):
         else:
             aux[count_nums[sum(int(c) if c.isdigit() else ord(c) for c in num)] - 1] = a[i]
             count_nums[sum(int(c) if c.isdigit() else ord(c) for c in num)] += 1
-    a = aux
+    # Runtime copying the sorted info back: O(n)
+    # Space Complexity: O(1)
+    for i, num in enumerate(aux):
+        a[i] = num
     return a
     # Total runtime: O(n) + O(n) + O(k) + O(k*logk) + O(n) --> O(3n) + O(2k*logk) --> O(n + k*logk)
     # Total space: O(n) + O(k) + O(1) + O(1) --> O(n + k) + 2 where k <= n always --> O(n)
 
+# Basic main function for testing and debugging. Not critical to the algorithm
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         data = f.read()
